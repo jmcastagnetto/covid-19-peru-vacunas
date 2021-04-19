640b48a7c982ea7f5e6d929cf084cff73a649f3b
Jesus M. Castagnetto <jesus@castagnetto.com>
2021-04-19 13:27:37 -0500

    Actualizado automáticamente el 2021-04-19

--- actualizar-datos.sh
+++ actualizar-datos.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
-Rscript get-data.R
+#Rscript get-data.R
+now=`date -I`
+git commit -a --no-gpg-sign -m "Actualizado automáticamente el $now"
+
+
 #Rscript vacunados-covid19-plots.R
 #Rscript distribucion-dias-entre-dosis.R
