f92d1438d34530fc45f211c9fd0a99de7390aa80
Jesus M. Castagnetto <jesus@castagnetto.com>
2021-04-18 21:24:37 -0500

    modificando script

--- actualizar-datos.sh
+++ actualizar-datos.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 Rscript get-data.R
-Rscript vacunados-covid19-plots.R
-Rscript distribucion-dias-entre-dosis.R
+#Rscript vacunados-covid19-plots.R
+#Rscript distribucion-dias-entre-dosis.R
