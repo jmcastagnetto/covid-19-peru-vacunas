8ec46e0c838ddc3af07459490852e92d8a15c18c
Jesus M. Castagnetto <jesus@castagnetto.com>
2021-04-19 13:29:16 -0500

    preparando para actualización automática

--- actualizar-datos.sh
+++ actualizar-datos.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-#Rscript get-data.R
+Rscript get-data.R
 now=`date -I`
 git commit -a --no-gpg-sign -m "Actualizado automáticamente el $now"
-
+git push origin main
 
 #Rscript vacunados-covid19-plots.R
 #Rscript distribucion-dias-entre-dosis.R
