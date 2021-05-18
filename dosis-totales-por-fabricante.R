b1053346d917769ce7bf8ee225e08bded9d57079
Jesus M. Castagnetto <jesus@castagnetto.com>
2021-05-17 23:41:16 -0500

    Preparando anotaciones para el inicio de vacunación de 65+ años

--- dosis-totales-por-fabricante.R
+++ dosis-totales-por-fabricante.R
@@ -1,8 +1,10 @@
 library(tidyverse)
+library(ggtext)
 
 df <- readRDS("datos/vacunas_covid_resumen.rds")
 fecha_corte <- unique(df$fecha_corte)
-
+min_date <- min(df$fecha_vacunacion, na.rm = TRUE)
+max_date <- max(df$fecha_vacunacion, na.rm = TRUE)
 max_y <- sum(df$n_reg)
 
 por_fabricante <- df %>%
@@ -20,60 +22,43 @@ por_fabricante <- df %>%
   ) %>%
   arrange(fecha_vacunacion, fabricante)
 
-ggplot(
+anotaciones <- tribble(
+  ~x, ~y, ~label, ~description,
+  as.Date("2021-03-08"), .75*max_y, "2021-03-08", "Inicio de vacunación de adultos mayores a 80 años",
+  as.Date("2021-04-30"), .75*max_y, "2021-04-30", "Inicio de vacunación de adultos mayores a 70 años",
+  as.Date("2021-05-21"), .75*max_y, "2021-05-21", "Inicio de vacunación de adultos mayores a 65 años"
+)
+
+p1 <- ggplot(
   por_fabricante,
   aes(x = fecha_vacunacion, y = n_acum,
       group = fabricante,
       fill = fabricante)
 ) +
   geom_area(color = "black") +
-  geom_vline(xintercept = as.Date("2021-03-08"),
-             color = "grey 40",
-             size = 2, linetype = "dashed") +
-  annotate(
-    geom = "text",
-    x = as.Date("2021-02-20"),
-    y = .95*max_y,
-    hjust = .5,
-    vjust = 1,
-    label = "2021-03-08:\nInicio de vacunación de\nadultos mayores a 80 años",
-    size = 7,
-    color = "grey40"
+  geom_vline(
+    data = anotaciones,
+    aes(xintercept = x),
+    color = "grey40",
+    size = 2,
+    linetype = "dashed"
   ) +
-  annotate(
-    geom = "curve",
-    x = as.Date("2021-02-20"),
-    y = .75*max_y,
-    xend = as.Date("2021-03-07"),
-    yend = .8e6,
-    angle = 45,
-    size = 1,
-    arrow = arrow(length = unit(5, "mm"), type = "closed"),
-    color = "grey40"
-  ) +
-  geom_vline(xintercept = as.Date("2021-04-30"),
-             color = "grey 40",
-             size = 2, linetype = "dashed") +
-  annotate(
-    geom = "text",
-    x = as.Date("2021-04-01"),
-    y = .95*max_y,
-    hjust = .5,
-    vjust = 1,
-    label = "2021-04-30:\nInicio de vacunación de\nadultos mayores a 70 años",
-    size = 7,
-    color = "grey40"
-  ) +
-  annotate(
-    geom = "curve",
-    x = as.Date("2021-04-01"),
-    y = .75*max_y,
-    xend = as.Date("2021-04-29"),
-    yend = .8e6,
-    angle = 45,
-    size = 1,
-    arrow = arrow(length = unit(5, "mm"), type = "closed"),
-    color = "grey40"
+  ggforce::geom_mark_circle(
+    data = anotaciones,
+    aes(
+      x = x,
+      y = y,
+      label = label,
+      description = description
+    ),
+    label.fill = rgb(1,1,1, .7),
+    con.type = "straight",
+    con.colour = "blue",
+    color = "blue",
+    fill = "blue",
+    alpha = 1,
+    expand = unit(1, "mm"),
+    inherit.aes = FALSE
   ) +
   scale_y_continuous(labels = scales::comma) +
   scale_x_date(
@@ -81,13 +66,13 @@ ggplot(
     date_labels = "Sem: %V\n%b %Y",
     expand = expansion()
   ) +
-  scale_fill_brewer(type = "qual", palette = "Pastel2") +
+  scale_fill_brewer(type = "qual", palette = "Set2") +
   labs(
     x = "",
     y = "Dosis totales aplicadas",
     fill = "",
     title = "Perú - Vacunación contra COVID-19",
-    subtitle = glue::glue("Fuente: Datos abiertos del MINSA (al {fecha_corte})"),
+    subtitle = glue::glue("*Fuente*: Datos abiertos del MINSA (del {min_date} al {max_date})"),
     caption = glue::glue("@jmcastagnetto, Jesus M. Castagnetto ({Sys.Date()})")
   ) +
   ggthemes::theme_hc(
@@ -97,14 +82,14 @@ ggplot(
   theme(
     plot.title.position = "plot",
     plot.title = element_text(size = 32, face = "bold"),
-    plot.subtitle = element_text(face = "italic", size = 22, colour = "grey70"),
+    plot.subtitle = element_markdown(size = 22, colour = "grey70"),
     plot.caption.position = "plot",
     plot.caption = element_text(family = "Inconsolata"),
     legend.position = "top",
     plot.margin = unit(rep(1, 4), "cm")
   )
-
 ggsave(
+  plot = p1,
   filename = "plots/dosis-totales-por-fabricante.png",
   width = 16,
   height = 10
