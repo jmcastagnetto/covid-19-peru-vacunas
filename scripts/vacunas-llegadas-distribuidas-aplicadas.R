20fde244bdcb62fa966f717289cb361be2165ed9
Jesus M. Castagnetto <jesus@castagnetto.com>
2021-07-13 11:47:16 -0500

    actualizando comparación de vacuanas llegadas, distribuidas y aplicadas

--- scripts/vacunas-llegadas-distribuidas-aplicadas.R
+++ scripts/vacunas-llegadas-distribuidas-aplicadas.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(ggtext)
+library(directlabels)
 #library(ggforce)
 
 vacunas <- readRDS("datos/vacunas_covid_resumen.rds") %>%
@@ -11,6 +12,20 @@ vacunas <- readRDS("datos/vacunas_covid_resumen.rds") %>%
   mutate(
     dosis_acum = cumsum(n_dosis)
   )
+
+vacunas_por_fabricante <- readRDS("datos/vacunas_covid_resumen.rds") %>%
+  group_by(fecha_vacunacion, fabricante) %>%
+  summarise(
+    n_dosis = sum(n_reg)
+  ) %>%
+  ungroup() %>%
+  arrange(fabricante, fecha_vacunacion) %>%
+  group_by(fabricante) %>%
+  mutate(
+    dosis_acum = cumsum(n_dosis)
+  )
+
+
 
 distribuidas <- readRDS("datos/vacunas_covid_distribucion.rds") %>%
   group_by(periodo) %>%
@@ -34,38 +49,53 @@ plot_df <- read_csv("datos/covid19_vaccine_arrivals_peru.csv") %>%
     )
   )
 
-# tbl <- plot_df %>%
-#   select(
-#     "Fecha de Llegada" = fecha_de_llegada,
-#     Fabricante = col_lbl,
-#     Cantidad = cantidad) %>%
-#   mutate(
-#     Cantidad = format(Cantidad, big.mark = ",")
-#  )
-
-max_y <- round_any(max(plot_df$cantidad_acumulada), 2.5e6)
+max_y <- max(plot_df$cantidad_acumulada) * 1.2
 
 max_date <- max(plot_df$fecha_de_llegada, vacunas$fecha_vacunacion)
+min_date <- min(plot_df$fecha_de_llegada, vacunas$fecha_vacunacion)
 vac_apl <- max(vacunas$dosis_acum)
 vac_arr <- max(plot_df$cantidad_acumulada)
 vac_dist <- max(distribuidas$n_acum)
 
-txt_annotation <- glue::glue("Al {max_date}, **han llegado al pais un total de {str_trim(format(vac_arr, big.mark = ','))} dosis** de vacunas, <span style='color:magenta;'>se han distribuído unas {str_trim(format(vac_dist, big.mark = ','))}</span>, de las cuales ya <span style='color:green;'>se han aplicado {str_trim(format(vac_apl, big.mark = ','))}</span> a nivel nacional")
 
 Sys.setlocale("LC_TIME", "es_PE.utf8")
 p1 <- ggplot() +
   geom_line(
     data = vacunas,
     aes(x = fecha_vacunacion, y = dosis_acum),
-    size = 2,
-    color = "green"
+    size = 1,
+    color = "darkgreen"
+  ) +
+  geom_dl(
+    data = vacunas %>% tail(1),
+    aes(
+      x = fecha_vacunacion,
+      y = dosis_acum,
+      label = glue::glue(
+        "Aplicadas: {format(dosis_acum, big.mark = ',')} dosis"
+      )
+    ),
+    color = "darkgreen",
+    method = list("last.bumpup", cex = 1.7)
   ) +
   geom_step(
     data = distribuidas,
     aes(x = periodo, y = n_acum),
     direction = "vh",
     size = 1,
-    color = "magenta"
+    color = "darkmagenta"
+  ) +
+  geom_dl(
+    data = distribuidas %>% tail(1),
+    aes(
+      x = periodo,
+      y = n_acum,
+      label = glue::glue(
+        "Distribuidas: {format(n_acum, big.mark = ',')} dosis"
+      )
+    ),
+    color = "darkmagenta",
+    method = list("last.bumpup", cex = 1.7)
   ) +
   geom_step(
     data = plot_df,
@@ -73,52 +103,56 @@ p1 <- ggplot() +
     direction = "vh",
     size = 1
   ) +
-  # geom_mark_circle(
-  #   data = plot_df,
-  #   aes(x = fecha_de_llegada,
-  #       y = cantidad_acumulada,
-  #       group = fecha_de_llegada,
-  #       label = farmaceutica,
-  #       color = farmaceutica,
-  #       description = paste0(format(cantidad, big.mark = ","), " dosis")
-  #   ),
-  #   expand = unit(1, "mm"),
-  #   con.colour = "blue",
-  #   label.fill = rgb(1, 1, 1, .6),
-  #   show.legend = FALSE
-  # ) +
-  geom_textbox(
-    aes(x = as.Date("2021-02-15"),
-        y = 0.8 * max_y,
-        label = txt_annotation),
-    size = 9,
-    width = unit(6, "in"),
-    vjust = 1,
-    hjust = 0,
-    box.color = NA,
-    inherit.aes = FALSE
+  geom_dl(
+    data = plot_df %>% tail(1),
+    aes(
+      x = fecha_de_llegada,
+      y = cantidad_acumulada,
+      label = glue::glue(
+        "Llegaron: {format(cantidad_acumulada, big.mark = ',')} dosis"
+      )
+    ),
+    method = list("last.bumpup", cex = 1.7)
   ) +
-  scale_y_continuous(labels = scales::comma, limit = c(0, max_y)) +
+  geom_area(
+    data = vacunas_por_fabricante,
+    aes(x = fecha_vacunacion, y = dosis_acum, fill = fabricante)
+  ) +
+  scale_y_continuous(labels = scales::comma) + #, limits = c(0, max_y)) +
+  scale_x_date(
+    date_breaks = "1 month",
+    date_labels = "%b",
+    limits = c(min_date, max_date + 45)
+  ) +
+  #scale_fill_brewer(palette = "Pastel2") +
+  scale_fill_manual(
+    values = c("#bae4b3", "#74c476", "#238b45")
+  ) +
   labs(
     x = "",
     y = "Número de dosis",
-    title = "COVID-19: Vacunación en Perú - Dosis entregadas, distribuídas y aplicadas",
-    subtitle = "Fuentes: MINSA, CENARES y Wikipedia",
-    caption = glue::glue("@jmcastagnetto, Jesus M. Castagnetto, {Sys.Date()}")
+    title = "COVID-19: Vacunación en Perú - Dosis que llegaron, se distribuyeron y aplicaron",
+    subtitle = glue::glue("Fuentes: MINSA, CENARES y Wikipedia. Datos al {max_date}."),
+    caption = glue::glue("@jmcastagnetto, Jesus M. Castagnetto, {Sys.Date()}"),
+    fill = "Fabricante de la\nvacuna aplicada"
   ) +
   theme_classic(base_size = 18) +
   theme(
     plot.title.position = "plot",
     plot.title = element_text(size = 28),
     plot.subtitle = element_text(size = 22, color = "grey40"),
-    plot.caption = element_text(family = "Inconsolata", size = 18)
+    plot.caption = element_text(family = "Inconsolata", size = 18),
+    legend.position = c(.9, .5),
+    legend.key.height = unit(1, "cm")
   )
+
 
 ggsave(
   plot = p1,
   filename = "plots/covid19-peru-vacunas-llegadas-distribuidas-aplicadas.png",
-  width = 16,
+  width = 20,
   height = 12
 )
+
 
 
