aecca1ad8d33e0b739b0c73f848172a8ae63be0d
Jesus M. Castagnetto <jesus@castagnetto.com>
2021-07-27 21:31:45 -0500

    actualizando el script de automatización

--- actualizar-datos.sh
+++ actualizar-datos.sh
@@ -1,21 +1,19 @@
 #! /bin/bash
 
-wget -O datos/orig/vacunas_covid.csv  https://cloud.minsa.gob.pe/s/ZgXoXqK2KLjRLxD/download
-md5sum --status -c md5sum.txt
+# use aria2c multithreaded multiple connection download
+aria2c -c -x8 -d datos/orig/ -o vacunas_covid.7z --file-allocation=falloc https://cloud.minsa.gob.pe/s/To2QtqoNjKqobfw/download
+sha256sum --status -c sha256sum.txt
 
 if [ $? -eq 0 ]
 then
 	echo "Los datos no han cambiado"
-	rm datos/orig/vacunas_covid.csv
+	rm datos/orig/vacunas_covid.7z
 else
 	echo "Datos han cambiado... Procesando ..."
-	md5sum datos/orig/vacunas_covid.csv > md5sum.txt
-	gzip -9 -f datos/orig/vacunas_covid.csv
+	md5sum datos/orig/vacunas_covid.7z > md5sum.txt
 	Rscript get-data.R
 	now=`date -I`
-	git commit -a -m "Actualizado el $now"
-	HOME=/home/jesus git push origin main
+    git add datos
+	git commit -a -m "Actualizado automáticamente el $now"
+	git push origin main
 fi
-
-#Rscript vacunados-covid19-plots.R
-#Rscript distribucion-dias-entre-dosis.R
