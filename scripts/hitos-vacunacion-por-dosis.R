f3a445e675a1d6a6864e42b587c38b8b2dacdf7b
Jesus M. Castagnetto <jesus@castagnetto.com>
2021-08-01 19:17:03 -0500

    gráfico de vacunados por cada millón y dosis

--- scripts/hitos-vacunacion-por-dosis.R
+++ scripts/hitos-vacunacion-por-dosis.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(patchwork)
+library(ggalt)
 
 vacunas <- readRDS("datos/vacunas_covid_resumen.rds")
 pe <- Sys.setlocale("LC_TIME", "es_PE.utf8")
@@ -17,9 +18,11 @@ dosis1 <- vacunas %>%
   arrange(fecha_vacunacion) %>%
   mutate(
     n_ac = cumsum(n),
-    tramo = trunc(n_ac / 5e5),
+    tramo = trunc(n_ac / 1e6),
     n_days = as.integer(fecha_vacunacion - min(fecha_vacunacion))
   )
+
+max_fechavac1 <- max(dosis1$fecha_vacunacion)
 
 markers_1 <- dosis1 %>%
   group_by(tramo) %>%
@@ -27,64 +30,63 @@ markers_1 <- dosis1 %>%
     fecha = min(fecha_vacunacion),
     n_ac = min(n_ac),
     dias = min(n_days)
+  ) %>%
+  ungroup() %>%
+  mutate(
+    tramo_lbl = glue::glue(
+      "{format({as.integer(tramo) * 1000000L}, big.mark = ',', format = 'd', trim = TRUE)} - {format({as.integer(tramo + 1) * 1000000L}, big.mark = ',', format = 'd', trim = TRUE)}"
+    ),
+    fecha_next = lead(fecha),
+    fecha_next = replace_na(fecha_next, max_fechavac1),
+    dias_diff = as.numeric(fecha_next - fecha),
+    fecha_middle = fecha + (dias_diff / 2)
   )
 
-inicio <- min(dosis1$fecha_vacunacion)
-
-p1 <- ggplot() +
-  geom_line(
-    data = dosis1,
-    aes(x = fecha_vacunacion, y = n_ac),
-    size = 1
-  ) +
-  geom_point(
-    data = markers_1 %>% filter(tramo > 0),
-    aes(x = fecha, y = n_ac),
-    color = "blue"
-  ) +
-  geom_segment(
-    data = markers_1 %>% filter(tramo > 0),
-    aes(x = fecha, y = 0,
-        xend = fecha, yend = n_ac),
-    linetype = "dashed",
-    color = "gray70"
-  ) +
-  geom_segment(
-    data = markers_1 %>% filter(tramo > 0),
-    aes(x = inicio,
-        y = n_ac,
-        xend = fecha,
-        yend = n_ac),
-    linetype = "dashed",
-    color = "gray70"
-  ) +
-  ggforce::geom_mark_circle(
-    data = markers_1 %>% filter(tramo > 0),
-    aes(x = fecha, y = n_ac, group = tramo,
-        label = glue::glue("{str_trim(format(n_ac, big.mark = ','))}\npersonas\nen {dias} días")),
-    color = "blue",
-    con.cap = 0,
-    con.colour = "blue",
-    expand = unit(2, "mm")
+p1 <- ggplot(
+  markers_1
+) +
+  geom_dumbbell(
+    aes(y = tramo_lbl, x = fecha, xend = fecha_next),
+    colour_x = "green",
+    colour_xend = "black",
+    size_x = 4,
+    size_xend = 4,
+    size = 2,
+    dot_guide = TRUE,
+    dot_guide_colour = "gray70",
+    dot_guide_size = 1
   ) +
   geom_text(
-    aes(x = as.Date("2021-03-15"), y = 2.5e6),
+    aes(
+      y = tramo_lbl,
+        x = fecha_middle,
+        label = glue::glue("{dias_diff} días")
+    ),
+    nudge_y = 0.15,
+    size = 6
+  ) +
+  annotate(
+    geom = "text",
     label = "Primera dosis",
-    size = 10,
-    hjust = 0
+    size = 12,
+    x = as.Date("2021-07-01"),
+    y = 2.5
   ) +
-  scale_y_continuous(
-    labels = scales::comma,
-    n.breaks = 10,
-    limits = c(0, 5e6)
+  scale_x_date(
+    date_breaks = "1 month",
+    date_labels = "%b"
   ) +
-  ggthemes::theme_tufte(base_size = 16) +
+  theme_classic() +
   labs(
     x = "",
     y = ""
+  ) +
+  theme(
+    axis.text = element_text(size = 14)
   )
 
-# Segunda dosis -----------------------------------------------------------
+
+# Segundas dosis -----------------------------------------------------------
 
 dosis2 <- vacunas %>%
   filter(dosis == 2) %>%
@@ -96,9 +98,11 @@ dosis2 <- vacunas %>%
   arrange(fecha_vacunacion) %>%
   mutate(
     n_ac = cumsum(n),
-    tramo = trunc(n_ac / 5e5),
+    tramo = trunc(n_ac / 1e6),
     n_days = as.integer(fecha_vacunacion - min(fecha_vacunacion))
   )
+
+max_fechavac2 <- max(dosis2$fecha_vacunacion)
 
 markers_2 <- dosis2 %>%
   group_by(tramo) %>%
@@ -106,69 +110,69 @@ markers_2 <- dosis2 %>%
     fecha = min(fecha_vacunacion),
     n_ac = min(n_ac),
     dias = min(n_days)
+  ) %>%
+  ungroup() %>%
+  mutate(
+    tramo_lbl = glue::glue(
+      "{format({as.integer(tramo) * 1000000L}, big.mark = ',', format = 'd', trim = TRUE)} - {format({as.integer(tramo + 1) * 1000000L}, big.mark = ',', format = 'd', trim = TRUE)}"
+    ),
+    fecha_next = lead(fecha),
+    fecha_next = replace_na(fecha_next, max_fechavac2),
+    dias_diff = as.numeric(fecha_next - fecha),
+    fecha_middle = fecha + (dias_diff / 2)
   )
 
-inicio2 <- min(dosis2$fecha_vacunacion)
-
-p2 <- ggplot() +
-  geom_line(
-    data = dosis2,
-    aes(x = fecha_vacunacion, y = n_ac),
-    size = 1
-  ) +
-  geom_point(
-    data = markers_2 %>% filter(tramo > 0),
-    aes(x = fecha, y = n_ac),
-    color = "blue"
-  ) +
-  geom_segment(
-    data = markers_2 %>% filter(tramo > 0),
-    aes(x = fecha, y = 0,
-        xend = fecha, yend = n_ac),
-    linetype = "dashed",
-    color = "gray70"
-  ) +
-  geom_segment(
-    data = markers_2 %>% filter(tramo > 0),
-    aes(x = inicio2,
-        y = n_ac,
-        xend = fecha,
-        yend = n_ac),
-    linetype = "dashed",
-    color = "gray70"
-  ) +
-  ggforce::geom_mark_circle(
-    data = markers_2 %>% filter(tramo > 0),
-    aes(x = fecha, y = n_ac, group = tramo,
-        label = glue::glue("{str_trim(format(n_ac, big.mark = ','))}\npersonas\nen {dias} días")),
-    color = "blue",
-    con.cap = 0,
-    con.colour = "blue",
-    expand = unit(2, "mm")
-  ) +
-  scale_y_continuous(
-    labels = scales::comma,
-    n.breaks = 10,
-    limits = c(0, 5e6)
+p2 <- ggplot(
+  markers_2 %>% slice(-nrow(markers_2))
+) +
+  geom_dumbbell(
+    aes(y = tramo_lbl, x = fecha, xend = fecha_next),
+    colour_x = "green",
+    colour_xend = "black",
+    size_x = 4,
+    size_xend = 4,
+    size = 2,
+    dot_guide = TRUE,
+    dot_guide_colour = "gray70",
+    dot_guide_size = 1
   ) +
   geom_text(
-    aes(x = as.Date("2021-03-15"), y = 2.5e6),
-    label = "Segunda dosis",
-    size = 10,
-    hjust = 0
+    aes(
+      y = tramo_lbl,
+      x = fecha_middle,
+      label = glue::glue("{dias_diff} días")
+    ),
+    nudge_y = 0.15,
+    size = 6
   ) +
-  ggthemes::theme_tufte(base_size = 16) +
+  annotate(
+    geom = "text",
+    label = "Segunda dosis",
+    size = 12,
+    x = as.Date("2021-07-01"),
+    y = 2.5
+  ) +
+  scale_x_date(
+    date_breaks = "1 month",
+    date_labels = "%b"
+  ) +
+  theme_classic() +
   labs(
     x = "",
     y = ""
+  ) +
+  theme(
+    axis.text = element_text(size = 14)
   )
+
+
 
 
 # Combinados --------------------------------------------------------------
 
 p12 <- (p1 + p2) +
   plot_annotation(
-    title = "COVID-19, Perú: Personas Vacunadas con Primera y Segunda Dosis",
+    title = "COVID-19, Perú: Cuánto tiempo ha tomado el vacunar cada millón de personas",
     subtitle = "Fuente de datos: MINSA",
     caption = glue::glue("@jmcastagnetto, Jesus M. Castagnetto ({Sys.Date()})")
   ) &
@@ -182,6 +186,6 @@ p12 <- (p1 + p2) +
 ggsave(
   p12,
   filename = "plots/hitos-vacunados-por-dosis.png",
-  width = 24,
-  height = 14
+  width = 22,
+  height = 10
 )
