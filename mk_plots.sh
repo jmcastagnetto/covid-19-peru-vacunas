9487801d7b9aad0ce08ad18e67a1897a598ccd89
Jesus M. Castagnetto <jesus@castagnetto.com>
2021-09-01 21:58:27 -0500

    cleanup step 2

--- mk_plots.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-Rscript viz/distribucion-vacunados-edades-por-departamento-dosis.R
-Rscript viz/dosis-totales-por-fabricante.R
-Rscript viz/pctpob_vacunados_rango_edades.R
-Rscript viz/rango-edades-sexo-plot.R
-Rscript viz/vacunados-covid19-plots.R
-Rscript viz/vacunas-llegadas-distribuidas-aplicadas.R
