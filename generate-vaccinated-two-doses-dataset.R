5db82d1c1bf41954aa67af9447ac1615e0de08c0
Jesus M. Castagnetto <jesus@castagnetto.com>
2021-09-02 08:49:17 -0500

    regenerando datos de vacunados con 2 dosis

--- generate-vaccinated-two-doses-dataset.R
+++ generate-vaccinated-two-doses-dataset.R
@@ -1,78 +1,39 @@
 library(tidyverse)
-#library(qs)
-
-ubigeos <- readRDS(url("https://github.com/jmcastagnetto/ubigeo-peru-aumentado/raw/main/ubigeos_reniec_inei_aumentado.rds")) %>%
-  select(reniec, inei, departamento, provincia, distrito) %>%
-  mutate_at(
-    vars(departamento, provincia, distrito),
-    iconv,
-    to='ASCII//TRANSLIT'
-  )
 
 vacunas <- readRDS("datos/vacunas_covid_aumentada.rds") %>%
-  select(uuid, sexo, rango_edad, dosis, fecha_vacunacion,
-         departamento, provincia, distrito) %>%
-  mutate(
-    provincia = str_replace_all(
-      provincia,
-      pattern = c(
-        "SAN ROMAS" = "SAN ROMAN"
-      )
-    ) %>%
-      iconv(to='ASCII//TRANSLIT'),
-    distrito = str_replace_all(
-      distrito,
-      pattern = c(
-        "MAGDALENA VIEJA \\(PUEBLO LIBRE\\)" = "PUEBLO LIBRE",
-        "LURIGANCHO \\(CHOSICA\\)" = "LURIGANCHO",
-        "KIMBIRI" = "QUIMBIRI",
-        "PICHANAKI" = "PICHANAQUI",
-        "ALEXANDER VON HUMBO" = "ALEXANDER VON HUMBOLDT",
-        "VEINTISEIS DE OCTUB" = "VEINTISEIS DE OCTUBRE",
-        "DANIEL ALOMIA ROBLES" = "DANIEL ALOMIAS ROBLES",
-        "SAN JUAN DE ISCOS" = "SAN JUAN DE YSCOS"
-      )
-    ) %>%
-      iconv(to='ASCII//TRANSLIT'),
-    # caso especial
-    provincia = if_else(
-      provincia == "CONCEPCION" &
-        distrito == "SANTO DOMINGO DE ACOBAMBA",
-      "HUANCAYO",
-      provincia
-    )
-  ) %>%
-  left_join(
-    ubigeos,
-    by = c("departamento", "provincia", "distrito")
-  )
+  select(id_persona, sexo, edad, dosis, 
+         fecha_vacunacion, persona_ubigeo = ubigeo_persona,
+         centro_vacunacion_ubigeo)
 
 dosis1 <- vacunas %>%
   filter(dosis == 1) %>%
   rename(
-    reniec1 = reniec,
-    inei1 = inei,
-    rango_edad1 = rango_edad,
-    dosis1 = fecha_vacunacion
+    vacunacion_ubigeo_1 = centro_vacunacion_ubigeo,
+    edad_1 = edad,
+    dosis_1 = fecha_vacunacion
   ) %>%
-  select(-dosis, -departamento, -provincia, -distrito)
+  select(-dosis)
 
 dosis2 <- vacunas %>%
   filter(dosis == 2) %>%
   rename(
-    reniec2 = reniec,
-    inei2 = inei,
-    rango_edad2 = rango_edad,
-    dosis2 = fecha_vacunacion
+    vacunacion_ubigeo_2 = centro_vacunacion_ubigeo,
+    edad_2 = edad,
+    dosis_2 = fecha_vacunacion
   ) %>%
-  select(-dosis, -departamento, -provincia, -distrito)
+  select(-dosis, -sexo, -persona_ubigeo)
 
 dos_dosis <- dosis1 %>%
   full_join(
     dosis2,
-    by = c("uuid", "sexo")
+    by = c("id_persona")
   ) %>%
-  filter(!is.na(dosis2))
+  filter(!is.na(dosis_2))
+
+saveRDS(
+  dos_dosis,
+  file = "datos/vacunados-dos-dosis.rds"
+)
 
 n_limit <- 1e6  # de millón en millón
 n_rows <- nrow(dos_dosis)
@@ -85,12 +46,3 @@ if (n_rows > n_limit) {
     write_csv(tmp_df, file = csvname)
   }
 }
-
-saveRDS(
-  dos_dosis,
-  file = "datos/vacunados-dos-dosis.rds"
-)
-#qsave(
-#  dos_dosis,
-#  file = "datos/vacunados-dos-dosis.qs"
-#)
