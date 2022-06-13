96ca7a9711e1f6d54026a42f044268c8ed215c2b
Jesus M. Castagnetto <jesus@castagnetto.com>
2022-06-13 19:00:03 +0300

    mods to account for the source data changes

--- 00-preproc-data.R
+++ 00-preproc-data.R
@@ -1,46 +1,29 @@
 library(dplyr, warn.conflicts = FALSE)
-library(vroom, warn.conflicts = FALSE)
-library(lubridate, warn.conflicts = FALSE)
 library(cli, warn.conflicts = FALSE)
 library(arrow, warn.conflicts = FALSE)
-library(collapse, warn.conflicts = FALSE, quietly = TRUE)
+library(lubridate)
 
 options(cli.progress_show_after = 0)
 options(cli.progress_clear = FALSE)
 
 cli_h1("Pre-procesando los datos")
 
-# separar datos por año y semana epi usando arrow
-cli_progress_step("Leyendo los datos originales y agregando epi_year, epi_week, last/first day of epi_week")
+cli_progress_step("Leyendo los datos originales")
 
-# vroom tiene mejor performance que readr::read_csv() o
-# que arrow::read_csv_arrow()
-vac_raw <- vroom(
-  "datos/orig/vacunas_covid.csv",
-  col_types = cols(
-    .default = col_integer(),
-    FECHA_VACUNACION = col_date(format = "%Y%m%d")
-  ),
-  num_threads = 4,
-  progress = TRUE,
-  altrep = TRUE
+vac_raw <- read_csv_arrow(
+  "datos/orig/vacunas_covid.csv"
 ) %>%
   janitor::clean_names() %>%
-  ftransform(
-    epi_year = epiyear(fecha_vacunacion) %>% as.integer(),
-    epi_week = epiweek(fecha_vacunacion) %>% as.integer(),
+  mutate(
+    fecha_corte = ymd(fecha_corte),
+    fecha_vacunacion = ymd(fecha_vacunacion),
+    epi_year = epiyear(fecha_vacunacion),
+    epi_week = epiweek(fecha_vacunacion),
     first_day_of_epi_week = floor_date(fecha_vacunacion,
                                        "weeks",
-                                       week_start = 7) # first dow
-  ) %>%
-  mutate(
+                                       week_start = 7), # first dow
     last_day_of_epi_week = first_day_of_epi_week + 6 # last dow
   )
-cli_progress_step(paste0(">>> Orig. rows: ", nrow(vac_raw)))
-#vac_raw <- distinct(vac_raw)
-#cli_progress_step(paste0(">>> Distinct rows: ", nrow(vac_raw)))
-
-cli_progress_step("Estructura de datos")
 str(vac_raw)
 
 cli_progress_step("Guardando datos por epi_year y epi_week como parquet files")
