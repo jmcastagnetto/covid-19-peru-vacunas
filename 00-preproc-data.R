8637d09451e96df21e7fb6fa2d4b9b1d86b3e2b8
Jesus M. Castagnetto <jesus.castagnetto@gmail.com>
2022-06-20 10:14:00 +0300

    Revert "assuming data has unique entries"
    This reverts commit 7da2a9afdd16667f32451cbc4591ad6f8b4957a0.

--- 00-preproc-data.R
+++ 00-preproc-data.R
@@ -24,9 +24,9 @@ vac_raw <- read_csv_arrow(
                                        week_start = 7), # first dow
     last_day_of_epi_week = first_day_of_epi_week + 6 # last dow
   )
-#cli_progress_step(paste0(">>> Orig. rows: ", nrow(vac_raw)))
-#vac_raw <- distinct(vac_raw)
-#cli_progress_step(paste0(">>> Distinct rows: ", nrow(vac_raw)))
+cli_progress_step(paste0(">>> Orig. rows: ", nrow(vac_raw)))
+vac_raw <- distinct(vac_raw)
+cli_progress_step(paste0(">>> Distinct rows: ", nrow(vac_raw)))
 
 cli_progress_step("Estructura de datos")
 str(vac_raw)
