1cff7be22560b6c87795b3f4ff68fcf1aeef904b
Jesus M. Castagnetto <jesus@castagnetto.com>
2022-08-22 10:02:00 -0500

    modified to simplify the output schemas for validation

--- validate-output.sh
+++ validate-output.sh
@@ -1,13 +1,9 @@
 #! /bin/bash
 
-frictionless validate --schema schemas/covid19_vaccine_arrivals_peru-schema.yaml datos/covid19_vaccine_arrivals_peru.csv
 frictionless validate --schema schemas/vacunas_covid_fabricante-schema.yaml datos/vacunas_covid_fabricante.csv
 frictionless validate --schema schemas/vacunas_covid_rangoedad_deciles-schema.yaml datos/vacunas_covid_rangoedad_deciles.csv
 frictionless validate --schema schemas/vacunas_covid_rangoedad_owid-schema.yaml datos/vacunas_covid_rangoedad_owid.csv
 frictionless validate --schema schemas/vacunas_covid_rangoedad_quintiles-schema.yaml datos/vacunas_covid_rangoedad_quintiles.csv
 frictionless validate --schema schemas/vacunas_covid_rangoedad_veintiles-schema.yaml datos/vacunas_covid_rangoedad_veintiles.csv
 frictionless validate --schema schemas/vacunas_covid_resumen-schema.yaml datos/vacunas_covid_resumen.csv
-frictionless validate --schema schemas/vacunas_covid_totales_fabricante_ubigeo-schema.yaml datos/vacunas_covid_totales_fabricante_ubigeo.csv
 frictionless validate --schema schemas/vacunas_covid_totales_por_semana-schema.yaml datos/vacunas_covid_totales_por_semana.csv
-frictionless validate --schema schemas/vacunas_covid_vacunafests-schema.yaml datos/vacunas_covid_vacunafests.csv 
-frictionless validate --schema schemas/vacunas_covid_vacunatones-schema.yaml datos/vacunas_covid_vacunatones.csv
