09295b81d3e3a0220b746ed3811b857b90f7449c
Jesus M. Castagnetto <jesus@castagnetto.com>
2022-11-28 01:47:15 +0000

    some comments and minor code rearrangements

--- update-duckdb.sh
+++ update-duckdb.sh
@@ -2,17 +2,18 @@
 
 eval $(keychain --agents gpg)
 
+# figure out the current data URL using the dkan API
 dataurl=`curl -s 'https://www.datosabiertos.gob.pe/api/3/action/package_show?id=24af4ff4-226d-4e3d-90cb-d26a1849796e' |  jq '.result| map(.resources)| .[] | .[] | .url' | head -1 | tr -d '"'`
 echo "Datos del $dataurl"
 echo ""
 mv datos/orig/vacunas_covid.7z datos/orig/vacunas_covid-prev.7z
-# aria2c -c -x4 -d datos/orig --force-save -o vacunas_covid.7z --file-allocation=falloc $dataurl
+# get the data including the appropriate headers to it lowers the risk of been blocked
 curl -o datos/orig/vacunas_covid.7z -A "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/81.0" --referer 'https://www.datosabiertos.gob.pe/dataset/vacunaci%C3%B3n-contra-covid-19-ministerio-de-salud-minsa' $dataurl
 dwnlstat=$?
 if [ $dwnlstat -ne 0 ];
 then
-	echo "** Error descargando los datos, intentar luego **"
-	exit $dwnlstat
+  echo "** Error descargando los datos, intentar luego **"
+  exit $dwnlstat
 fi
 
 ls -lh datos/orig/vacunas_covid.7z
@@ -21,6 +22,7 @@ sha256sum --status -c sha256sum.txt
 if [ $? -eq 0 ]
 then
   echo "Los datos no han cambiado"
+  exit 1
 else
   today=`date +%Y-%m-%d`
   echo "Datos han cambiado"
@@ -37,11 +39,11 @@ else
   fstatus=$?
   if [ $fstatus -eq 0 ]
   then
-	rm tmp/sample_vacunas.csv
-	echo ">> Muestra de 1M de datos de entrada validados"
+    rm tmp/sample_vacunas.csv
+    echo ">> Muestra de 1M de datos de entrada validados"
   else
-	echo "** Error: Datos de entrada no tienen el formato esperado **"
-	exit $fstatus
+    echo "** Error: Datos de entrada no tienen el formato esperado **"
+    exit $fstatus
   fi
   # Load data into duckdb
   echo "Backup de BD previa"
@@ -55,22 +57,23 @@ else
   # Validate output data
   echo "Validando datos"
   ( frictionless validate --schema schemas/vacunas_covid_fabricante-schema.yaml datos/vacunas_covid_fabricante.csv && \
-  frictionless validate --schema schemas/vacunas_covid_rangoedad_deciles-schema.yaml datos/vacunas_covid_rangoedad_deciles.csv && \
-  frictionless validate --schema schemas/vacunas_covid_rangoedad_owid-schema.yaml datos/vacunas_covid_rangoedad_owid.csv && \
-  frictionless validate --schema schemas/vacunas_covid_rangoedad_quintiles-schema.yaml datos/vacunas_covid_rangoedad_quintiles.csv && \
-  frictionless validate --schema schemas/vacunas_covid_rangoedad_veintiles-schema.yaml datos/vacunas_covid_rangoedad_veintiles.csv && \
-  frictionless validate --schema schemas/vacunas_covid_resumen-schema.yaml datos/vacunas_covid_resumen.csv && \
-  frictionless validate --schema schemas/vacunas_covid_totales_por_semana-schema.yaml datos/vacunas_covid_totales_por_semana.csv )
+    frictionless validate --schema schemas/vacunas_covid_rangoedad_deciles-schema.yaml datos/vacunas_covid_rangoedad_deciles.csv && \
+    frictionless validate --schema schemas/vacunas_covid_rangoedad_owid-schema.yaml datos/vacunas_covid_rangoedad_owid.csv && \
+    frictionless validate --schema schemas/vacunas_covid_rangoedad_quintiles-schema.yaml datos/vacunas_covid_rangoedad_quintiles.csv && \
+    frictionless validate --schema schemas/vacunas_covid_rangoedad_veintiles-schema.yaml datos/vacunas_covid_rangoedad_veintiles.csv && \
+    frictionless validate --schema schemas/vacunas_covid_resumen-schema.yaml datos/vacunas_covid_resumen.csv && \
+    frictionless validate --schema schemas/vacunas_covid_totales_por_semana-schema.yaml datos/vacunas_covid_totales_por_semana.csv )
   outvalstat=$?
   if [ $outvalstat -ne 0 ]
   then
-  	echo "** Validación de los datos ha fallado, revisar antes de publicar **"
-  	exit $outvalstat
+    echo "** Validación de los datos ha fallado, revisar antes de publicar **"
+    exit $outvalstat
   else
-  	echo "Estado del repo"
-  	git status
-  	tail datos/vacunas_covid_resumen.csv
-  	git commit -a -m "Datos al $today (automated processing)"
-  	git push origin main
+    echo "Estado del repo"
+    git status
+    tail datos/vacunas_covid_resumen.csv
+    git commit -a -m "Datos al $today (automated processing)"
+    git push origin main
   fi
+  exit 0
 fi
