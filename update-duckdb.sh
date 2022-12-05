672cee0b5c4670a79b5e845f46d2306aa1e3fe9f
Jesus M. Castagnetto <jesus@castagnetto.com>
2022-12-05 22:55:20 +0000

    Datos al 2022-12-04, procesados automáticamente el 2022-12-05

--- update-duckdb.sh
+++ update-duckdb.sh
@@ -29,55 +29,69 @@ else
   sha256sum datos/orig/vacunas_covid.7z > sha256sum.txt
   echo "Extrayendo el CSV"
   7z e -aoa -odatos/orig/ datos/orig/vacunas_covid.7z
-  ls -lh datos/orig/
-  head -5 datos/orig/vacunas_covid.csv
-  echo "Validando datos de entrada"
-  # Validate input data
-  # sample 1M entries to validate
-  xsv sample 1000000 datos/orig/vacunas_covid.csv > tmp/sample_vacunas.csv
-  frictionless validate --type resource --schema schemas/vacunas_covid-orig-schema.yaml tmp/sample_vacunas.csv
-  fstatus=$?
-  if [ $fstatus -eq 0 ]
+  if [ $? -eq 0 ]
   then
-    rm tmp/sample_vacunas.csv
-    echo ">> Muestra de 1M de datos de entrada validados"
+    ls -lh datos/orig/
+    head -5 datos/orig/vacunas_covid.csv
+    echo "Validando datos de entrada"
+    # Validate input data
+    # sample 1M entries to validate
+    xsv sample 1000000 datos/orig/vacunas_covid.csv > tmp/sample_vacunas.csv
+    frictionless validate --type resource --schema schemas/vacunas_covid-orig-schema.yaml tmp/sample_vacunas.csv
+    fstatus=$?
+    if [ $fstatus -eq 0 ]
+    then
+      rm tmp/sample_vacunas.csv
+      echo ">> Muestra de 1M de datos de entrada validados"
+    else
+      echo "** Error: Datos de entrada no tienen el formato esperado **"
+      exit $fstatus
+    fi
+    # Load data into duckdb
+    echo "Backup de BD previa"
+    mv tmp/ddb/peru-vacunas-covid19.duckdb tmp/ddb/peru-vacunas-covid19-backup.duckdb
+    echo "Inicializando duckdb"
+    Rscript create-duckdb-tables.R
+    echo "Cargando en duckdb"
+    duckdb -init tmp/ddb/duckdb-config.sql tmp/ddb/peru-vacunas-covid19.duckdb < tmp/ddb/duckdb-load-csv.sql
+    ddbexit=$?
+    if [ $ddbexit -eq 0 ]
+    then
+      echo "Removing extracted CSV file"
+      rm datos/orig/vacunas_covid.csv
+      echo "Generando los resúmenes"
+      Rscript process-data-from-duckdb.R
+      # Get the cut-off date using the "-list" option as "-ascii" produces an RS (0x1e) char
+      fcorte=`duckdb -list -noheader -c "SELECT FECHA_CORTE FROM vacunas_proc LIMIT 1;" tmp/ddb/peru-vacunas-covid19.duckdb`
+      # Just in case, there is a need to remove RS 0x1e in the future
+      # fcorte=$(echo $fcorte | sed 's/[\x1e]//g')
+      # Validate output data
+      echo "Validando datos"
+      ( frictionless validate --schema schemas/vacunas_covid_fabricante-schema.yaml datos/vacunas_covid_fabricante.csv && \
+        frictionless validate --schema schemas/vacunas_covid_rangoedad_deciles-schema.yaml datos/vacunas_covid_rangoedad_deciles.csv && \
+        frictionless validate --schema schemas/vacunas_covid_rangoedad_owid-schema.yaml datos/vacunas_covid_rangoedad_owid.csv && \
+        frictionless validate --schema schemas/vacunas_covid_rangoedad_quintiles-schema.yaml datos/vacunas_covid_rangoedad_quintiles.csv && \
+        frictionless validate --schema schemas/vacunas_covid_rangoedad_veintiles-schema.yaml datos/vacunas_covid_rangoedad_veintiles.csv && \
+        frictionless validate --schema schemas/vacunas_covid_resumen-schema.yaml datos/vacunas_covid_resumen.csv && \
+        frictionless validate --schema schemas/vacunas_covid_totales_por_semana-schema.yaml datos/vacunas_covid_totales_por_semana.csv )
+      outvalstat=$?
+      if [ $outvalstat -ne 0 ]
+      then
+        echo "** Validación de los datos ha fallado, revisar antes de publicar **"
+        exit $outvalstat
+      else
+        echo "Estado del repo"
+        git status
+        tail datos/vacunas_covid_resumen.csv
+      git commit -a -m "Datos al $fcorte, procesados automáticamente el $today"
+        git push origin main
+      fi
+    else
+      echo "** Error cargando los datos en duckdb **"
+    fi
   else
-    echo "** Error: Datos de entrada no tienen el formato esperado **"
-    exit $fstatus
-  fi
-  # Load data into duckdb
-  echo "Backup de BD previa"
-  mv tmp/ddb/peru-vacunas-covid19.duckdb tmp/ddb/peru-vacunas-covid19-backup.duckdb
-  echo "Inicializando duckdb"
-  Rscript create-duckdb-tables.R
-  echo "Cargando en duckdb"
-  duckdb -init tmp/ddb/duckdb-config.sql tmp/ddb/peru-vacunas-covid19.duckdb < tmp/ddb/duckdb-load-csv.sql
-  echo "Generando los resúmenes"
-  Rscript process-data-from-duckdb.R
-  # Get the cut-off date using the "-list" option as "-ascii" produces an RS (0x1e) char
-  fcorte=`duckdb -list -noheader -c "SELECT FECHA_CORTE FROM vacunas_proc LIMIT 1;" tmp/ddb/peru-vacunas-covid19.duckdb`
-  # Just in case, there is a need to remove RS 0x1e in the future
-  # fcorte=$(echo $fcorte | sed 's/[\x1e]//g')
-  # Validate output data
-  echo "Validando datos"
-  ( frictionless validate --schema schemas/vacunas_covid_fabricante-schema.yaml datos/vacunas_covid_fabricante.csv && \
-    frictionless validate --schema schemas/vacunas_covid_rangoedad_deciles-schema.yaml datos/vacunas_covid_rangoedad_deciles.csv && \
-    frictionless validate --schema schemas/vacunas_covid_rangoedad_owid-schema.yaml datos/vacunas_covid_rangoedad_owid.csv && \
-    frictionless validate --schema schemas/vacunas_covid_rangoedad_quintiles-schema.yaml datos/vacunas_covid_rangoedad_quintiles.csv && \
-    frictionless validate --schema schemas/vacunas_covid_rangoedad_veintiles-schema.yaml datos/vacunas_covid_rangoedad_veintiles.csv && \
-    frictionless validate --schema schemas/vacunas_covid_resumen-schema.yaml datos/vacunas_covid_resumen.csv && \
-    frictionless validate --schema schemas/vacunas_covid_totales_por_semana-schema.yaml datos/vacunas_covid_totales_por_semana.csv )
-  outvalstat=$?
-  if [ $outvalstat -ne 0 ]
-  then
-    echo "** Validación de los datos ha fallado, revisar antes de publicar **"
-    exit $outvalstat
-  else
-    echo "Estado del repo"
-    git status
-    tail datos/vacunas_covid_resumen.csv
-	git commit -a -m "Datos al $fcorte, procesados automáticamente el $today"
-    git push origin main
+    echo "** Error extrayendo el archivo de datos **"
+    exit $7zexit
   fi
   exit 0
 fi
